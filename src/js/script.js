1cf90894e380511d3d0fdd0dfbf98a62e5f7acdd
Richard-Nathan <richardnathannunes@gmail.com>
2025-03-15 14:32:32 -0300

    correção no form

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,9 @@
 
-function getLocalTimeInTimezone(timezone) {
+const getLocalTimeInTimezone=(timezone)=> {
     const date = new Date()
     return date.toLocaleString("pt-BR", { timeZone: timezone })
 }
-function update() {
+const update=()=> {
     const tokyoTimeElement = document.getElementById("hora")
     if (!tokyoTimeElement) return
     tokyoTimeElement.textContent = getLocalTimeInTimezone("Asia/Tokyo")
@@ -11,7 +11,7 @@ function update() {
 }
 let parar
 const mediaQuery = window.matchMedia("(max-width:550px)")
-function verificarTamanho(e) {
+const verificarTamanho=e=> {
     if (e.matches) {
         //matches é uma propriedade do objeto retornado(uma mediaquery) quando seu usa matchmedia,
         //no qual verifica se a condição (tamanho) é verdadeira.
@@ -25,9 +25,14 @@ function verificarTamanho(e) {
 verificarTamanho(mediaQuery)
 mediaQuery.addEventListener("change", verificarTamanho)
 
-
-function nada() {
+document.getElementById("form").addEventListener("submit", event=>{
+    event.preventDefault()
     let nome = document.getElementById('inome').value
     alert(`Desculpe ${nome}, ainda não aprendi essa parte :)`)
-   
-}
+} )
+
+
+// const nada=()=> {
+//     let nome = document.getElementById('inome').value
+//     alert(`Desculpe ${nome}, ainda não aprendi essa parte :)`)
+// }
